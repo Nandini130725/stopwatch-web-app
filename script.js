6e02ca18749218d17bb655c6bdc4ff65ec285dde
Nandini130725 <nandinikowlepally13@gmail.com>
2025-06-24 20:57:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 0, secs = 0, mins = 0;
+let timerId = null;
+
+startBtn.addEventListener('click', function() {
+    if (timerId !== null) {
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function() {
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function() {
+    clearInterval(timerId);
+    timerDisplay.innerHTML = "00 : 00 : 00";
+    msec = secs = mins = 0;
+});
+
+function startTimer() {
+    msec++;
+    if (msec === 100) {
+        msec = 0;
+        secs++;
+    }
+    if (secs === 60) {
+        secs = 0;
+        mins++;
+    }
+
+    timerDisplay.innerHTML =
+        (mins < 10 ? "0" + mins : mins) + " : " +
+        (secs < 10 ? "0" + secs : secs) + " : " +
+        (msec < 10 ? "0" + msec : msec);
+}
